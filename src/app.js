47160bd43cd4605a1208547abbc43bbc1109372c
aaransmith11 <aaransmith0817@gmail.com>
2025-03-01 16:58:48 +0000

    refactored

--- src/app.js
+++ src/app.js
@@ -6,19 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.querySelector(".btn").addEventListener("click" , generateExcuse)
+  document.querySelector(".btn").addEventListener("click" , generateExcuse);
 };
 const generateExcuse =()=>{
-  let subject =["The cat", "My grandma", "That kid", "The Mailman", "The demented bunny"]
-  let action = ["shot", "microwaved", "ate", "stole", "burnt"]
-  let object = ["my car", "the house", "my computer", "my phone", "my mom"]
-  let subjectIndex = Math.floor(Math.random()*subject.length)
-  let actionIndex = Math.floor(Math.random()*action.length)
-  let objectIndex = Math.floor(Math.random()*object.length)
-  let randomSubject = subject[subjectIndex]
-  let randomAction = action[actionIndex]
-  let randomObject = object[objectIndex]
-  document.querySelector(".subject").innerHTML = randomSubject
-  document.querySelector(".action").innerHTML = randomAction
-  document.querySelector(".object").innerHTML = randomObject
+  // arrays represent each part of the excuse. 
+  let subject =["The cat", "My grandma", "That kid", "The Mailman", "The demented bunny", "The Sasquatch"];
+  let action = ["shot", "microwaved", "ate", "stole", "burnt" , "abducted"];
+  let object = ["my car", "the house", "my computer", "my phone", "my mom", "the bike"];
+
+  // randomize the index
+  let randomSubject = subject[Math.floor(Math.random()*subject.length)];
+  let randomAction = action[Math.floor(Math.random()*action.length)];
+  let randomObject = object[Math.floor(Math.random()*object.length)];
+
+  // replace existing HTML with new excuse
+  document.querySelector(".subject").innerHTML = randomSubject;
+  document.querySelector(".action").innerHTML = randomAction;
+  document.querySelector(".object").innerHTML = randomObject;
 }
